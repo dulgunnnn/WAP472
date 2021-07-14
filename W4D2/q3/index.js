e63a4f9a1ef0210a47cb18915392e939df279153
dulguun <33824231+dulgunnnn@users.noreply.github.com>
2021-07-14 03:35:01 -0500

    w4d2

---
+++ W4D2/q3/index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(session({ secret: "random salt" }));
+app.use("/css", express.static(path.join(__dirname, "view/css")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "view"));
+app.use(express.json());
+app.use("/js", express.static(path.join(__dirname, "view", "js")));
+
+let products = [
+  {
+    name: "Book: Twenty Thousand Leagues Under the Seas",
+    price: "20",
+    description:
+      "The novel was originally serialized from March 1869 through June 1870 in Pierre-Jules Hetzel's fortnightly periodical, the Magasin d'éducation et de récréation",
+    id: 0,
+    quantity: 5,
+  },
+  {
+    name: "Mug: Marimekko ",
+    price: "15",
+    description:
+      "Marimekko's cups and mugs available both in monochrome and colourful Marimekko patterns, such as Räsymatto, Siirtolapuutarha and Unikko.",
+    id: 1,
+    quantity: 10,
+  },
+];
+
+let cartItemQuantity = 0;
+
+app.get("/", (req, res, next) => {
+  res.render("product", { products: products });
+});
+
+app.get("/shoppingcart", (req, res, next) => {
+  res.render("shoppingcart", {
+    products: req.session.products ? req.session.products : [],
+  });
+});
+
+app.post("/addToCart", (req, res, next) => {
+  if (req.session.products === undefined)
+    req.session.products = [
+      {
+        name: req.body.name,
+        price: req.body.price,
+        id: req.body.id,
+        quantity: 1,
+      },
+    ];
+  else {
+    let item = req.session.products.find(
+      (item) => item.id.toString() === req.body.id
+    );
+    if (item === undefined)
+      req.session.products.push({
+        name: req.body.name,
+        price: req.body.price,
+        id: req.body.id,
+        quantity: 1,
+      });
+    else item.quantity++;
+  }
+  cartItemQuantity++;
+
+  res.send(cartItemQuantity.toString());
+  res.end();
+});
+
+app.listen(3000);
